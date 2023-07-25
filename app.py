3b0e4aecb3334841e60d1320f914c502e9bb48b2
LECAE Thomas <thomas.lecae@inrae.fr>
2023-07-25 16:23:23 +0200

    using grid is better

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from streamlit_folium import folium_static
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load the result GeoDataFrame from the shapefile
-result_shapefile_path = os.path.join(current_dir, "data", "points_walk_results_.shp")
+result_shapefile_path = os.path.join(current_dir, "data", "grid_walk_results_.shp")  # Change the file name to your polygons shapefile
 result_gdf = gpd.read_file(result_shapefile_path)
 
 # Load the park GeoDataFrame from the shapefile
 park_shapefile_path = os.path.join(current_dir, "data", "public_parks.shp")
 park_gdf = gpd.read_file(park_shapefile_path)
 
-# Create a folium map centered at the mean of the result points
+# Create a folium map centered at the centroid of the first polygon in result_gdf
 m = folium.Map(location=[result_gdf["geometry"].centroid.y.mean(), result_gdf["geometry"].centroid.x.mean()], zoom_start=12)
 
 # Create a colormap for the walking times (gradient of reds)
@@ -23,29 +23,30 @@ min_walking_time = result_gdf["walking_ti"].min()
 max_walking_time = result_gdf["walking_ti"].max()
 color_scale = folium.LinearColormap(colors=["green", "yellow", "red"], vmin=min_walking_time, vmax=max_walking_time)
 
-
-# Add the points with walking times to the map
+# Add the polygons with walking times to the map using GeoJson
 for idx, row in result_gdf.iterrows():
-    point = row.geometry
+    polygon = row.geometry
     walking_time = row["walking_ti"]
-    folium.CircleMarker(
-        location=[point.y, point.x],
-        radius=6,
-        popup=f"Walking Time: {walking_time} seconds",
-        fill=True,
-        fill_color=color_scale(walking_time),
-        color=color_scale(walking_time),
-        fill_opacity=0.7,
+    park_cor = row["park_id"]
+    folium.GeoJson(
+        polygon.__geo_interface__,
+        style_function=lambda x, walking_time=walking_time: {
+            "fillColor": color_scale(walking_time),
+            "color": color_scale(walking_time),
+            "weight": 2,
+            "fillOpacity": 0.7,
+        },
+        tooltip=f"Walking Time: {walking_time} seconds  <br> ID du parc correspondant : {park_cor}",  # Add tooltip for the polygons
     ).add_to(m)
 
 # Add park polygons to the map
 for idx, row in park_gdf.iterrows():
     park_id = row["id"]
-    park_polygon = row.geometry.__geo_interface__
+    park_polygon = row.geometry
     folium.GeoJson(
-        park_polygon,
+        park_polygon.__geo_interface__,
         style_function=lambda x: {"fillColor": "transparent", "color": "blue", "weight": 2},
-        tooltip=f"Park ID: {park_id}",
+        tooltip=f"Park ID: {park_id}",  # Add tooltip for the parks
         name=f"Park {park_id}",
     ).add_to(m)
 
