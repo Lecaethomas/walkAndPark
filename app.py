29a22644978cb7cb3ea740149fd7936ab1eba7e2
Thomas Lecaé <46902922+Lecaethomas@users.noreply.github.com>
2023-07-26 11:09:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ st.title("Tentative de modélisation de l'accessibilité des parcs urbains toulo
 st.subheader("Quoi? :deciduous_tree:" )
 
 # Add some normal text describing the map
-st.write("Cette carte montre l'accessibilité à des parcs calculée pour des personnes non-PMR depuis le centre des carrés vers points représentant les parcs. Les carreaux font 200m*200m et correspondent à la base de données Filosofi de l'INSEE filtrée sur la commune de Toulouse, laquelle permet de connaître les revenus moyens des ménages à l'échelle de cette unité qu'est le carreau. Un carreau signifie donc la présence de ménages mais suffisamment nombreux pour garantir leur anonymat.")
+st.write("Cette carte montre l'accessibilité à des parcs calculée pour des personnes non-PMR depuis le centre des carrés vers points représentant les parcs. Les carreaux font 200m*200m et correspondent à la base de données Filosofi de l'INSEE filtrée sur la commune de Toulouse. Cette base de données fournit différents indicateurs socio-économiques à l'échelle de cette unité qu'est le carreau. Un carreau signifie donc la présence de ménages mais suffisamment nombreux pour garantir leur anonymat.")
 st.write("Les temps de déplacements sont représentés en utilisant une échelle de couleurs allant du jaune au marron, la première correspondant à un temps cours, la deuxième à un temps long.")
 
 # Display the folium map in the Streamlit app using streamlit_folium's folium_static function
@@ -91,7 +91,7 @@ st.caption("Algo :computer:" )
 st.write("Il y a certainement la possibilité d'aller plus vite qu'actuellement en explorant les options qu'offre l'OSRM (le traitement total prend 9h actuellement avec une instance OSRM sur une machine en local).")
 st.write("Les géométries des parcs sont récupérées sous forme de polygones puis résumées sous forme de centroïdes, ce qui pose des problèmes de représentativité pour des parcs de grand taille comme Pech-David.")
 st.caption("Data-visualisation :chart:"	)
-st.write("Côté dataviz il serait sympa de pouvoir cliquer sur un carreau et que le parc correspondant pop (par un highlight ou autre) mais pour l'instant je n'ai pas réussi à trouver la solution.")
+st.write("Côté dataviz il serait sympa de pouvoir cliquer sur un carreau et que le parc correspondant pop (par un highlight ou autre) mais pour l'instant je n'ai pas réussi à trouver la solution avec folium.")
 st.write("L'idéal serait de passer sous Mapbox.js, qui offre une plus grande accessibilité, de créer une interface dédiée, de servir les géométries avec une API........")
 st.write("Quitte à refaire le calcul, on pourrait garder le nom des parcs.")
 
