67b17fb5cdc662a4aee46b22472cc3f0aa01f897
LECAE Thomas <thomas.lecae@inrae.fr>
2023-07-26 08:56:53 +0200

    add data for the whole town

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ from streamlit_folium import folium_static
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load the result GeoDataFrame from the shapefile
-result_shapefile_path = os.path.join(current_dir, "data", "grid_walk_results_.shp")  # Change the file name to your polygons shapefile
+result_shapefile_path = os.path.join(current_dir, "data", "grid_walk_results.shp")  # Change the file name to your polygons shapefile
 result_gdf = gpd.read_file(result_shapefile_path)
 
 # Load the park GeoDataFrame from the shapefile
-park_shapefile_path = os.path.join(current_dir, "data", "public_parks.shp")
+park_shapefile_path = os.path.join(current_dir, "data", "public_parks_.shp")
 park_gdf = gpd.read_file(park_shapefile_path)
 
 # Create a folium map centered at the centroid of the first polygon in result_gdf
@@ -57,23 +57,30 @@ color_scale.add_to(m)
 
 ############ TEXT
 # Add a title to the app
-st.title("Analyse de l'accessibilité des parcs urbains toulousains")
+st.title("Tentative de modélisation de l'accessibilité des parcs urbains toulousains")
 st.subheader("Quoi? :deciduous_tree:" )
 # Add some normal text describing the map
-st.write("Cette carte montre l'accessibilité à des parcs calculée pour des personnes valides depuis le centre des carrés. Ces derniers font 200m*200m et correspondent à la base de données Filosofi de l'INSEE, laquelle permet de connaître les revenus moyens des ménages à l'échelle de cette unité qu'est le carreau (ce qui laisse entrevoir des analyses intéressantes). Ce carroyage se base sur les revenus fiscaux des foyers et ne recense de carreau que là où des foyers existent d'une part et que l'on peut garantir leur anonymat d'autre part")
-st.write("Les temps de déplacements sont représentés en utilisant une échelle de couleurs allant du jaune au marron, la première correspondant à un temps cours, la deuxième à un temps long")
+st.write("Cette carte montre l'accessibilité à des parcs calculée pour des personnes valides depuis le centre des carrés. Ces derniers font 200m*200m et correspondent à la base de données Filosofi de l'INSEE, laquelle permet de connaître les revenus moyens des ménages à l'échelle de cette unité qu'est le carreau (ce qui laisse entrevoir des analyses intéressantes). Ce carroyage se base sur les revenus fiscaux des foyers et ne laisse des carreaux que là où des foyers existent d'une part et uniquement là où l'on peut garantir leur anonymat d'autre part")
+st.write("Les temps de déplacements sont représentés en utilisant une échelle de couleurs allant du jaune au marron, la première correspondant à un temps cours, la deuxième à un temps long.")
 
 # Display the folium map in the Streamlit app using streamlit_folium's folium_static function
 folium_static(m)
+
+st.write("Au delà de la question de l'accessibilité à la ressource spécifique que sont les espaces verts, ce type d'analyse peut être conduit pour tout types de données (Base Permanente des Equipements, BD Topo ...) et permettre ainsi d'avoir un aperçut de la dotation des territoires en équipements ainsi que leur accessibilité voir d'aborder le [concept de la ville du 1/4 d'heure](https://www.moreno-web.net/wordpress/wp-content/uploads/2020/12/Livre-Blanc-2-Etude-ville-quart-heure-18.12.2020.pdf).")
 st.subheader("Comment? :question:" )
-st.write("Techniquement le principe derrière cette modélisation est que l'on récupère le centroïde de chaque carreau, pour ensuite faire appel à l'API overpass (données OSM) permettant de récupérer les polygones correspondant aux parcs (leisure = parks) dans un rayon de 1km.")
-st.write("L'algorithme fait ensuite appel à l'OSRM (Open Source Routing Machine) pour chaque point et chaque parc correspondant pour calculer le temps nécessaire pour le rejoindre à pieds (modalité 'Route'). On peut ensuite extraire le parc le plus proche ainsi que le temps nécessaire pour s'y rendre.")
+st.write("Techniquement le principe derrière cette modélisation est que l'on récupère le centroïde de chaque carreau, pour ensuite faire appel à l'API overpass (données OSM) permettant de récupérer les polygones correspondant aux parcs (leisure = parks) dans un rayon de 1km autour de chaque point.")
+st.write("L'algorithme fait ensuite appel à l'OSRM (Open Source Routing Machine) pour chaque point et les parcs correspondants pour calculer le temps nécessaire pour les rejoindre à pieds (modalité 'Route'). On peut ensuite extraire le parc le plus proche ainsi que le temps nécessaire pour s'y rendre.")
 st.subheader("Améliorations :construction:")
 st.caption("Algo :computer:" )
-st.write("En travaillant avec l'OSRM j'ai découvert qu'il était possible de calculer le plus proche élément (modalité 'Nearest') entre un point de départ et un set de points d'arrivée, ce qui permettrait de trouver directement le chemin le plus court")
+st.write("Il y a certainement la possibilité d'aller plus vite qu'actuellement en explorant les possibilités qu'offre l'OSRM")
 st.caption("Data-visualisation :chart:"	)
 st.write("Côté dataviz il serait sympa de pouvoir cliquer sur un carreau et que le parc correspondant pop (par un highlight ou autre) mais pour l'instant je n'ai pas réussi à trouver la solution.")
 st.write("Quitte à refaire le calcul, on pourrait garder le nom des parcs...")
+st.subheader("Autres")
+st.caption("Les sources :white_check_mark:" )
+st.write("Filosofi (Fichier Localisé Social et Fiscal), carreaux 200m | INSEE - 2015")
+st.write("Parcs OSM | API Overpass - utilisée le 22/07/2023")
+st.write("Réseau viaire OSM | GEOFABRIK - màj le 24/07/2023")
 st.caption("Le code :cat2:")
 st.write("[Le repo pour les calculs est ici](https://github.com/Lecaethomas/walkAndPark_backend/tree/master)")
 st.write("[Le repo pour la dataviz est ici](https://github.com/Lecaethomas/walkAndPark_backend/tree/master)")
